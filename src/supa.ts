8bf0ab772d5fb33d6391896cbd1f0c35af33cb22
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-03-26 14:27:39 -0400

    supa

--- src/supa.ts
+++ src/supa.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://hdbbdxcqjzezwxvhfczj.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkYmJkeGNxanplend4dmhmY3pqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk4MzgxMDYsImV4cCI6MTk5NTQxNDEwNn0.NrfRxrLyEUKXGogaxXmWIhoFBRQ0BSRJI6Fi8cnqYgw';
 
 export const supabase = createClient(
   supabaseUrl as string,
